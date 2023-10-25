2b71f5e77bb8f8b0517166956d7f2f36cc479140
JBisquick <felipebirch@gmail.com>
2023-10-24 20:07:36 -0700

    Add gameController module pattern and mark placement

--- script.js
+++ script.js
@@ -1,21 +1,71 @@
 const gameBoard = (function () {
-  let board = ['X', 'O', 'X', 'O', 'X', 'X','O', 'O','X'];
-  return {board};
+  let board = new Array(9).fill('');
+
+  const getBoard = () => board;
+
+  const addMark = function(e) {
+    board[e.target.getAttribute('data-index')] = gameController.getCurrentPlayer().getMark();
+    e.target.textContent = board[e.target.getAttribute('data-index')]
+    gameController.update();
+
+  };
+
+  return {
+    getBoard,
+    addMark};
 })();
+
 
 const displayController = (function () {
+  const displayBoard = () => {
+    for (let i = 0; i < 9; i++) {
+      const gridCell = document.querySelector(`[data-index="${i}"]`); 
+      gridCell.textContent = gameBoard.getBoard()[i];
+      
+      if (gameBoard.getBoard()[i] === '') {
+        gridCell.addEventListener('click', gameBoard.addMark);
+      }
+    }
+  };
 
+  return {
+    displayBoard};
 })();
 
-function createPlayer (name) {
 
-}
+const gameController = (function () {
+  let currentPlayer = createPlayer('X');
+  let nexPlayer = createPlayer('O');
 
-function displayGameBoard (board) {
-  for (let i = 0; i < 9; i++) {
-    const gridCell = document.getElementById(`${i}`); 
-    gridCell.textContent = board[i];
-  }
-}
+  const startGame = () => {
+    displayController.displayBoard();
+  };
 
-displayGameBoard(gameBoard.board);
+  const getCurrentPlayer = () => currentPlayer;
+
+  const update = () => {
+    for (let i = 0; i < 9; i++) {
+      const gridCell = document.querySelector(`[data-index="${i}"]`); 
+      gridCell.removeEventListener('click', gameBoard.addMark);
+    }
+
+    [currentPlayer, nexPlayer] = [nexPlayer, currentPlayer];
+
+    displayController.displayBoard();
+  };
+
+  return {
+  startGame,
+  getCurrentPlayer,
+  update};
+})();
+
+
+function createPlayer(mark) {
+  const getMark = () => mark; 
+
+  return {
+    getMark}
+};
+
+ gameController.startGame();
